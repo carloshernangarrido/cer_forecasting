25e6b33bdce4981ce57e7018fb95113e253d375e
Hernán <carloshernangarrido@gmail.com>
2022-10-10 16:56:21 -0300

    not cached

--- pages/03_💵_Validación_Dólar_Blue.py
+++ pages/03_💵_Validación_Dólar_Blue.py
@@ -4,7 +4,6 @@ from utils.common import forecast_dolar_blue_prophet, common_dash, common_data
 from utils.plots import df2plot, plot_df_fc
 from utils.pre_processing import get_uva_df, get_day_diff
 
-
 if __name__ == '__main__':
     option_uva_cer, option_delta_years, option_days_ahead, option_origin = common_dash()
     cer_df, uva_df, cer_df_fc, uva_df_fc, today, dolar_blue_df, dolar_blue_df_fc = \
@@ -13,7 +12,7 @@ if __name__ == '__main__':
     date_val = (today - dt.timedelta(days=option_days_ahead))
     dolar_blue_df_val = dolar_blue_df[:date_val]
     dolar_blue_df_fc_val = forecast_dolar_blue_prophet(df_actual=dolar_blue_df_val,
-                                                              days_ahead=option_days_ahead, dump=False)
+                                                       days_ahead=option_days_ahead, dump=False)
 
     st.header(f"Validación de pronóstico de dólar blue")
     with st.expander(label="Valores", expanded=True):
@@ -32,4 +31,3 @@ if __name__ == '__main__':
         fig_day_val.add_vline(x=date_val)
         fig_day_val.add_annotation(x=date_val, y=df_plot.loc[date_val]['y'], text='hoy - horizonte')
         st.plotly_chart(fig_day_val, use_container_width=True)
-
